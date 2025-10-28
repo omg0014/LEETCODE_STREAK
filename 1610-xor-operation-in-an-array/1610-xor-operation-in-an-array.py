af9535446e2cd4f0fe7b5a6763feb5a12cb52a0f
Om Gupta <omgstar0014@gmail.com>
2025-10-28 11:43:52 +0530

    Time: 4 ms (3.61%), Space: 17.9 MB (31.87%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums=[]
+        for i in range(n):
+            nums.append(start+(2*i))
+        c=0
+        for i in nums:
+            c^=i
+        return c
+        
+        
