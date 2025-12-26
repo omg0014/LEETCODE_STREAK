1f3be03c6b822b77f386302346085046e2b24400
Om Gupta <omgstar0014@gmail.com>
2025-12-26 12:31:48 +0530

    Time: 0 ms (100%), Space: 17.2 MB (99.07%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words=s.split()
+        if len(pattern) != len(words):
+            return False
+    
+        for i in range(len(pattern)):
+            if pattern.index(pattern[i]) != words.index(words[i]):
+                return False
+        
+        return True
+            
