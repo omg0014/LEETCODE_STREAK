1b22daea29f4ca5c21186815365cec0b775c04dd
Om Gupta <omgstar0014@gmail.com>
2026-02-04 12:46:57 +0530

    Time: 3 ms (83.02%), Space: 19.3 MB (32.31%) - LeetHub

--- 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
+++ 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.py
@@ -5,16 +5,13 @@ class Solution:
         nums.append(avg)
         nums.sort()
         c=0
-        a=0
         if avg < 0:
             avg=1
-            
         for i in nums:
             if i<avg:
-                c+=1
+                continue
             elif i==avg :
                 avg+=1
-
             elif avg!=i and avg>0:
                 return avg
         return avg
