ef79519afe64232888352b66a1dd4af4677ae5bc
Om Gupta <omgstar0014@gmail.com>
2026-04-09 11:33:09 +0530

    Time: 19 ms (84.4%), Space: 26.5 MB (27.18%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        arr=[]
+        for i in nums:
+            arr.append(int(i))
+        arr.sort(reverse=True)
+        return str(arr[k-1])
+        
